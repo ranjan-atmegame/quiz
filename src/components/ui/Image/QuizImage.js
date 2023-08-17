df49ededbda7c6d0d529ee790844336e0986ac7a
vivekatmegame <mithilesh.thakur@atmegame.com>
2023-08-17 13:03:15 +0530

    new ui

--- src/components/ui/Image/QuizImage.js
+++ src/components/ui/Image/QuizImage.js
@@ -4,8 +4,8 @@ import { IMG_PATH } from '@/config';
 export default function QuizImage({
   imageName,
   name,
-  width = 54,
-  height = 58,
+  width = 70,
+  height = 70,
 }) {
   return (
     <Image
