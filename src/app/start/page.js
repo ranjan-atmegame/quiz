d40a3fe2a534829957d62c7d49cad336110c5540
ranjan-atmegame <ranjan.kumar@atmegame.com>
2023-08-22 15:49:15 +0530

    redirect start page

--- src/app/start/page.js
+++ src/app/start/page.js
@@ -1,8 +1,15 @@
 import dynamic from 'next/dynamic';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 const Start = dynamic(() => import('@/components/start'));
 const Layout = dynamic(() => import('@/components/ui/layout'));
 
 export default function Page() {
+  const isSubmitted = cookies().get('tq-submitted')?.value;
+  if (isSubmitted) {
+    redirect('/');
+  }
+
   return (
     <Layout isHeader={false}>
       <Start />
