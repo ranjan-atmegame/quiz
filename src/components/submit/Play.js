dcde256ca0bb1442baee75450164f8497aeee957
ranjan-atmegame <ranjan.kumar@atmegame.com>
2023-08-30 18:25:07 +0530

    update coins on submit page

--- src/components/submit/Play.js
+++ src/components/submit/Play.js
@@ -1,22 +1,16 @@
 'use client';
-import { useEffect } from 'react';
 import Link from 'next/link';
-import { updateUser } from '@/api/auth';
 import styles from './playNow.module.css';
 import Lottie from '../animation/Lottie';
 
-export default function Play() {
-  useEffect(() => {
-    updateUser({ countryCode: 'IN' });
-  }, []);
-
+export default function Play({ user }) {
   return (
     <div className={styles.playNow}>
       <div className={styles.stage}>
         <div className={`${styles.box} ${styles.bounce}`}></div>
       </div>
       <Lottie size="lottieCoinsSM" src={'/animation/CoinsAnimation.json'} />
-      <h1>You have got 200 coins</h1>
+      <h1>You have got {user.coins} coins</h1>
 
       <p>
         Check out more quizzes to test your skills and keeps grabbing more
