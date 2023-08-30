be5a1c565dd2ee6bac2380a8f63b732fe5eed81e
ranjan-atmegame <ranjan.kumar@atmegame.com>
2023-08-30 11:01:46 +0530

    pointed api to testing server

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 import { CONTEST_TYPES, CRICKET_SUBDOMAIN } from '@/utils/Constant';
 
 // export const API_URL = 'http://localhost:5000';
-export const API_URL = 'http://localhost:3001';
-// export const API_URL = 'https://testserv.atmegame.com';
+// export const API_URL = 'http://localhost:3001';
+export const API_URL = 'https://testserv.atmegame.com';
 export const LOCATION_API = 'https://pro.ip-api.com/json?key=iZun9ZnnZf8crvL';
 
 export const IMG_PATH = 'https://www.atmequiz.com';
