cd57a83337082f7a90bf8ffa0d1d22f0765fa0d1
ranjan-atmegame <ranjan.kumar@atmegame.com>
2023-09-06 10:19:09 +0530

    added log

--- src/components/rewardIcon/api.js
+++ src/components/rewardIcon/api.js
@@ -2,15 +2,15 @@ import { showRewardAd } from '@/utils';
 import { updateCoins } from '@/api/auth';
 import { DEFAULT_COIN } from '@/utils/Constant';
 
-export const updateRewardCoins = (coins = DEFAULT_COIN) => {
+export const updateRewardCoins = () => {
   showRewardAd((result) => {
     if (result?.status === 'viewed') {
       //
     } else if (result?.status === 'frequencyCapped') {
       // Display other ad
-      displayAd();
+      // displayAd();
     }
 
-    updateCoins(coins);
+    updateCoins(DEFAULT_COIN);
   });
 };
