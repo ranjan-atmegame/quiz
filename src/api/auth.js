23b1331cf14af7ac68c381d3bb4cacdb867a22f9
ranjan-atmegame <ranjan.kumar@atmegame.com>
2023-09-06 11:14:26 +0530

    remove log

--- src/api/auth.js
+++ src/api/auth.js
@@ -70,7 +70,6 @@ export const updateCoins = (coins) => {
   const auth = authenticate();
   const updatedCoins = auth.user.coins + coins;
   auth.user = { ...auth.user, coins: updatedCoins };
-  console.log({ user: auth.user, coins });
   saveAuth(auth);
   return updateCoins;
 };
