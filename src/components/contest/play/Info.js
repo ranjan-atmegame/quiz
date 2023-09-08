313640d7dab85ae96724188e42ad2a4bcfec814e
vivekatmegame <mithilesh.thakur@atmegame.com>
2023-09-08 21:09:03 +0530

    sound ui

--- src/components/contest/play/Info.js
+++ src/components/contest/play/Info.js
@@ -2,11 +2,14 @@
 import { formatNumber } from '@/utils';
 import CoinIcon from '@/components/coin';
 import styles from './info.module.css';
+import Image from 'next/image';
 
 export default function ContestInfo({ contest }) {
   return (
     <>
       <div className={styles.info}>
+        <div className={`${styles.sound}`}></div>
+        {/* <div className={`${styles.sound} ${styles.noSound}`}></div> */}
         <div className={`${styles.box}`}>
           <h3>{contest.name}</h3>
         </div>
