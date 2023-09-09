f220a3c03f3b8dc54eb84cfaab15bc0c4aba6385
ranjan-atmegame <ranjan.kumar@atmegame.com>
2023-09-09 08:52:57 +0530

    set API URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,14 @@
 import { CONTEST_TYPES, CRICKET_SUBDOMAIN } from '@/utils/Constant';
 
-// export const API_URL = 'http://localhost:5000';
-// export const API_URL = 'http://localhost:3001';
-export const API_URL = 'https://testserv.atmegame.com';
 export const LOCATION_API = 'https://pro.ip-api.com/json?key=iZun9ZnnZf8crvL';
 
+const API_LIST = {
+  PROD: process.env.API_URL,
+  TEST: process.env.TEST_API_URL,
+  DEV: process.env.DEV_API_URL,
+};
+
+export const API_URL = API_LIST[process.env.ENV];
 export const IMG_PATH = 'https://www.atmequiz.com';
 export const SECRET_KEY = 'atmequiz@1234567890#';
 
