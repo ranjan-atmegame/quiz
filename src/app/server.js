230c66609df8fe7ac592f70404ce9ab8f63b3215
ranjan-atmegame <ranjan.kumar@atmegame.com>
2023-09-12 12:27:57 +0530

    uncomment

--- src/app/server.js
+++ src/app/server.js
@@ -4,16 +4,20 @@ import { USER_LOCATION } from '@/utils/Constant';
 import { getCategory } from '@/api';
 
 export const getRobot = (indexPages) => {
-  // const headerList = headers();
-  // if (!headerList.headers.host.includes('www')) {
-  //   return { index: false, follow: false };
-  // }
+  const headerList = headers();
+  if (!headerList.headers.host.includes('www')) {
+    return { index: false, follow: false };
+  }
 
   if (indexPages) {
     return { index: true, follow: true };
   }
 
   return { index: false, follow: true };
+};
+
+export const getCanonicalUrl = (indexPages) => {
+  return '/';
 };
 
 export const getCategoryList = async () => {
