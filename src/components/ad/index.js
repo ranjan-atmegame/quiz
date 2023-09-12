ec69e35d60ca17ae3ec1e76739cf13c52fff226c
ranjan-atmegame <ranjan.kumar@atmegame.com>
2023-09-12 16:55:20 +0530

    fix ad issue

--- src/components/ad/index.js
+++ src/components/ad/index.js
@@ -1,13 +1,12 @@
 // import GoogleAd from './GoogleAd';
-import Ad from './Ad';
+import GoogleAd from './Ad';
 import { ad, sponsored } from '@/app/page.module.css';
 
 export default function Ad() {
   return (
     <div className={ad}>
       <span className={sponsored}>SPONSORED</span>
-      {/* <GoogleAd /> */}
-      <Ad />
+      <GoogleAd />
     </div>
   );
 }
